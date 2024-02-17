6234b36c8cbc1da79c0c50b5fe83e23e792fbdef
2noScript <thangmk7@gmail.com>
2024-02-18 03:35:49 +0700

    [Update] use lib fake-browser-headers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import express from "express";
 import axios from "axios";
-import NodeCache from "node-cache";
-import lodash from "lodash";
+import { browserHeader} from 'fake-browser-headers'
 import "dotenv/config";
 
-const { sample } = lodash;
 const port = process.env.PORT || 4003;
 const server = express();
-const cache = new NodeCache();
 
-const existenceTime = 3 * 60 * 60;
-const exBrowserHeaderTime = 20 * 60 * 60;
 
 server.get("/", (req, res, next) => {
   res.send("server on");
@@ -18,27 +13,6 @@ server.get("/", (req, res, next) => {
 
 server.get("/proxy", async (req, res, next) => {
   const { src, url } = req.query;
-  const cachedData = cache.get(url);
-
-  let browserHeader = cache.get("browser-header");
-
-  if (!browserHeader) {
-    for (let i = 0; i < 10; i++) {
-      try {
-        const { data } = await axios.get(
-          "https://http-support.vercel.app/generate-browser-headers?limit=100"
-        );
-        if (data) {
-          cache.set("browser-header", data, exBrowserHeaderTime);
-          browserHeader = data;
-          break;
-        }
-      } catch {
-        continue;
-      }
-    }
-  }
-
   if (!src) {
     res.status(400).json({
       message: "src is not empty",
@@ -47,47 +21,28 @@ server.get("/proxy", async (req, res, next) => {
     res.status(400).json({
       message: "url is not empty",
     });
-  } else if (cachedData) {
-    console.log("Data found in cache");
-    res.send(cachedData);
-  } else {
-    let response;
-    let error;
-    for (let i = 0; i < 10; i++) {
-      try {
-        const res = await axios.get(url, {
-          responseType: "arraybuffer",
+  }  else {
+     for(let i=0;i<5;i++){
+       try{
+        const response = await axios.get(url, {
+          responseType: "stream",
           headers: {
             Referer: src,
             Origin: src,
             "X-Requested-With": "XMLHttpRequest",
-            ...sample(browserHeader),
+           ...browserHeader()
           },
           timeout: 10000,
         });
-        response = res;
-        break;
-      } catch (err) {
-        error = err;
-        continue;
-      }
-    }
-
-    if (response && response?.data) {
-      const dataStream = response.data;
-      const contentType = response.headers["content-type"];
-      cache.set(url, dataStream, existenceTime);
-      res.setHeader("Content-Type", contentType);
-      res.send(dataStream);
-    } else if (error) {
-      console.error("Error fetching data:", error);
-      res.status(404).json({ error });
-    } else {
-      res.status(404).json({
-        message: "Fix bugs 😵‍💫",
-      });
-    }
-  }
+       response.data.pipe(res)                               
+       return
+       }
+       catch{
+       }
+     }}
+      res.status(500).json({
+        message:'fix bug'
+      })
 });
 server.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
