1217496319c3b6e88fb04afc96e4ab3e13c6cbe5
2noScript <thangmk7@gmail.com>
2024-03-27 11:19:36 +0700

    [Modify] change js to ts

--- src/index.js
+++ src/index.js
@@ -1,49 +1,49 @@
-import express from "express";
-import axios from "axios";
-import { browserHeader} from 'fake-browser-headers'
-import "dotenv/config";
+// import express from "express";
+// import axios from "axios";
+// import { browserHeader} from 'fake-browser-headers'
+// import "dotenv/config";
 
-const port = process.env.PORT || 4003;
-const server = express();
+// const port = process.env.PORT || 4003;
+// const server = express();
 
 
-server.get("/", (req, res, next) => {
-  res.send("server on");
-});
+// server.get("/", (req, res, next) => {
+//   res.send("server on");
+// });
 
-server.get("/proxy", async (req, res, next) => {
-  const { src, url } = req.query;
-  if (!src) {
-    res.status(400).json({
-      message: "src is not empty",
-    });
-  } else if (!url) {
-    res.status(400).json({
-      message: "url is not empty",
-    });
-  }  else {
-     for(let i=0;i<5;i++){
-       try{
-        const response = await axios.get(url, {
-          responseType: "stream",
-          headers: {
-            Referer: src,
-            Origin: src,
-            "X-Requested-With": "XMLHttpRequest",
-           ...browserHeader()
-          },
-          timeout: 10000,
-        });
-       response.data.pipe(res)                               
-       return
-       }
-       catch{
-       }
-     }}
-      res.status(500).json({
-        message:'fix bug'
-      })
-});
-server.listen(port, () => {
-  console.log(`server running on http://localhost:${port}`);
-});
+// server.get("/proxy", async (req, res, next) => {
+//   const { src, url } = req.query;
+//   if (!src) {
+//     res.status(400).json({
+//       message: "src is not empty",
+//     });
+//   } else if (!url) {
+//     res.status(400).json({
+//       message: "url is not empty",
+//     });
+//   }  else {
+//      for(let i=0;i<5;i++){
+//        try{
+//         const response = await axios.get(url, {
+//           responseType: "stream",
+//           headers: {
+//             Referer: src,
+//             Origin: src,
+//             "X-Requested-With": "XMLHttpRequest",
+//            ...browserHeader()
+//           },
+//           timeout: 10000,
+//         });
+//        response.data.pipe(res)                               
+//        return
+//        }
+//        catch{
+//        }
+//      }}
+//       res.status(500).json({
+//         message:'fix bug'
+//       })
+// });
+// server.listen(port, () => {
+//   console.log(`server running on http://localhost:${port}`);
+// });
--- src/new.js
+++ src/new.js
@@ -1,186 +1,186 @@
-import express from "express";
-import axios from "axios";
-const app = express();
+// import express from "express";
+// import axios from "axios";
+// const app = express();
 
-const PORT = process.env.PORT || 3002;
+// const PORT = process.env.PORT || 3002;
 
-// https://stackoverflow.com/questions/1714786/query-string-encoding-of-a-javascript-object
-const serialize = (obj) => {
-  const str = [];
-  for (const p in obj)
-    if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-    }
-  return str.join("&");
-};
+// // https://stackoverflow.com/questions/1714786/query-string-encoding-of-a-javascript-object
+// const serialize = (obj) => {
+//   const str = [];
+//   for (const p in obj)
+//     if (obj.hasOwnProperty(p)) {
+//       str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+//     }
+//   return str.join("&");
+// };
 
-// [['cookie', 'x-foo']] -> [["cookie", "x-foo"]]
-const parseHeaders = (stringHeaders) => {
-  try {
-    return JSON.parse(stringHeaders);
-  } catch {
-    try {
-      return JSON.parse(stringHeaders.replace(/'/g, '"'));
-    } catch {
-      return {};
-    }
-  }
-};
+// // [['cookie', 'x-foo']] -> [["cookie", "x-foo"]]
+// const parseHeaders = (stringHeaders) => {
+//   try {
+//     return JSON.parse(stringHeaders);
+//   } catch {
+//     try {
+//       return JSON.parse(stringHeaders.replace(/'/g, '"'));
+//     } catch {
+//       return {};
+//     }
+//   }
+// };
 
-// [["cookie", "x-foo"]] -> { cookie: "x-foo" }
-const composeHeaders = (arrayOfHeaders) => {
-  const headers = {};
+// // [["cookie", "x-foo"]] -> { cookie: "x-foo" }
+// const composeHeaders = (arrayOfHeaders) => {
+//   const headers = {};
 
-  arrayOfHeaders.forEach((header) => {
-    headers[header[0]] = header[1];
-  });
+//   arrayOfHeaders.forEach((header) => {
+//     headers[header[0]] = header[1];
+//   });
 
-  return headers;
-};
+//   return headers;
+// };
 
-// Parse string to its type
-const composeQuery = (originalQuery) => {
-  let query = originalQuery;
+// // Parse string to its type
+// const composeQuery = (originalQuery) => {
+//   let query = originalQuery;
 
-  if (originalQuery?.decompress) {
-    query.ignoreReqHeaders = originalQuery?.decompress === "true";
-  }
+//   if (originalQuery?.decompress) {
+//     query.ignoreReqHeaders = originalQuery?.decompress === "true";
+//   }
 
-  if (originalQuery?.ignoreReqHeaders) {
-    query.ignoreReqHeaders = originalQuery?.ignoreReqHeaders === "true";
-  }
+//   if (originalQuery?.ignoreReqHeaders) {
+//     query.ignoreReqHeaders = originalQuery?.ignoreReqHeaders === "true";
+//   }
 
-  if (originalQuery?.redirectWithProxy) {
-    query.ignoreReqHeaders = originalQuery?.redirectWithProxy === "true";
-  }
+//   if (originalQuery?.redirectWithProxy) {
+//     query.ignoreReqHeaders = originalQuery?.redirectWithProxy === "true";
+//   }
 
-  if (originalQuery?.followRedirect) {
-    query.followRedirect = originalQuery?.followRedirect === "true";
-  }
+//   if (originalQuery?.followRedirect) {
+//     query.followRedirect = originalQuery?.followRedirect === "true";
+//   }
 
-  if (originalQuery?.appendReqHeaders) {
-    const headers = parseHeaders(originalQuery.appendReqHeaders);
+//   if (originalQuery?.appendReqHeaders) {
+//     const headers = parseHeaders(originalQuery.appendReqHeaders);
 
-    query.appendReqHeaders = composeHeaders(headers);
-  }
+//     query.appendReqHeaders = composeHeaders(headers);
+//   }
 
-  if (originalQuery?.appendResHeaders) {
-    const headers = parseHeaders(originalQuery.appendResHeaders);
+//   if (originalQuery?.appendResHeaders) {
+//     const headers = parseHeaders(originalQuery.appendResHeaders);
 
-    query.appendResHeaders = composeHeaders(headers);
-  }
+//     query.appendResHeaders = composeHeaders(headers);
+//   }
 
-  if (originalQuery?.deleteReqHeaders) {
-    const headers = parseHeaders(originalQuery.deleteReqHeaders);
+//   if (originalQuery?.deleteReqHeaders) {
+//     const headers = parseHeaders(originalQuery.deleteReqHeaders);
 
-    query.deleteReqHeaders = headers;
-  }
+//     query.deleteReqHeaders = headers;
+//   }
 
-  if (originalQuery?.deleteResHeaders) {
-    const headers = parseHeaders(originalQuery.deleteResHeaders);
+//   if (originalQuery?.deleteResHeaders) {
+//     const headers = parseHeaders(originalQuery.deleteResHeaders);
 
-    query.deleteResHeaders = headers;
-  }
+//     query.deleteResHeaders = headers;
+//   }
 
-  return query;
-};
+//   return query;
+// };
 
-// https://bobbyhadz.com/blog/javascript-lowercase-object-keys
-const toLowerKeys = (obj) =>
-  Object.keys(obj).reduce((accumulator, key) => {
-    accumulator[key.toLowerCase()] = obj[key];
-    return accumulator;
-  }, {});
+// // https://bobbyhadz.com/blog/javascript-lowercase-object-keys
+// const toLowerKeys = (obj) =>
+//   Object.keys(obj).reduce((accumulator, key) => {
+//     accumulator[key.toLowerCase()] = obj[key];
+//     return accumulator;
+//   }, {});
 
-const concatHeaders = (...args) => {
-  const totalHeaders = {};
+// const concatHeaders = (...args) => {
+//   const totalHeaders = {};
 
-  for (const headers of args) {
-    Object.assign(totalHeaders, toLowerKeys(headers));
-  }
+//   for (const headers of args) {
+//     Object.assign(totalHeaders, toLowerKeys(headers));
+//   }
 
-  return totalHeaders;
-};
+//   return totalHeaders;
+// };
 
-app.get("/proxy", async (req, res) => {
-  const query = composeQuery(req.query);
+// app.get("/proxy", async (req, res) => {
+//   const query = composeQuery(req.query);
 
-  const {
-    url,
-    ignoreReqHeaders = false,
-    followRedirect = false,
-    redirectWithProxy = false,
-    decompress = false,
-    appendReqHeaders = {},
-    appendResHeaders = {},
-    deleteReqHeaders = [],
-    deleteResHeaders = [],
-  } = query;
+//   const {
+//     url,
+//     ignoreReqHeaders = false,
+//     followRedirect = false,
+//     redirectWithProxy = false,
+//     decompress = false,
+//     appendReqHeaders = {},
+//     appendResHeaders = {},
+//     deleteReqHeaders = [],
+//     deleteResHeaders = [],
+//   } = query;
 
-  if (!url) {
-    res.status(400).send("Missing url");
-    return;
-  }
+//   if (!url) {
+//     res.status(400).send("Missing url");
+//     return;
+//   }
 
-  const decodedUrl = decodeURIComponent(url);
+//   const decodedUrl = decodeURIComponent(url);
 
-  const host = new URL(decodedUrl).host;
+//   const host = new URL(decodedUrl).host;
 
-  let headers = concatHeaders({ host, ...appendReqHeaders });
+//   let headers = concatHeaders({ host, ...appendReqHeaders });
 
-  if (!ignoreReqHeaders) {
-    headers = concatHeaders(req.headers, headers);
-  }
+//   if (!ignoreReqHeaders) {
+//     headers = concatHeaders(req.headers, headers);
+//   }
 
-  const filteredHeaders = Object.keys(headers).reduce((acc, key) => {
-    if (!deleteReqHeaders.includes(key)) {
-      acc[key] = headers[key];
-    }
-    return acc;
-  }, {});
+//   const filteredHeaders = Object.keys(headers).reduce((acc, key) => {
+//     if (!deleteReqHeaders.includes(key)) {
+//       acc[key] = headers[key];
+//     }
+//     return acc;
+//   }, {});
 
-  const response = await axios.get(decodedUrl, {
-    responseType: "stream",
-    headers: filteredHeaders,
-    validateStatus: () => true,
-    maxRedirects: !followRedirect ? 0 : 5,
-    decompress,
-  });
+//   const response = await axios.get(decodedUrl, {
+//     responseType: "stream",
+//     headers: filteredHeaders,
+//     validateStatus: () => true,
+//     maxRedirects: !followRedirect ? 0 : 5,
+//     decompress,
+//   });
 
-  const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*",
-  };
+//   const corsHeaders = {
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Methods": "*",
+//   };
 
-  const resHeaders = concatHeaders(
-    response.headers,
-    corsHeaders,
-    appendResHeaders
-  );
+//   const resHeaders = concatHeaders(
+//     response.headers,
+//     corsHeaders,
+//     appendResHeaders
+//   );
 
-  for (let header in resHeaders) {
-    if (deleteResHeaders.includes(header.toLowerCase())) continue;
+//   for (let header in resHeaders) {
+//     if (deleteResHeaders.includes(header.toLowerCase())) continue;
 
-    if (header.toLowerCase() === "location") {
-      const originalUrl = resHeaders[header];
-      const encodedUrl = encodeURIComponent(originalUrl);
-      const redirectUrl = redirectWithProxy
-        ? `/proxy?url=${encodedUrl}&${serialize(query)}`
-        : originalUrl;
+//     if (header.toLowerCase() === "location") {
+//       const originalUrl = resHeaders[header];
+//       const encodedUrl = encodeURIComponent(originalUrl);
+//       const redirectUrl = redirectWithProxy
+//         ? `/proxy?url=${encodedUrl}&${serialize(query)}`
+//         : originalUrl;
 
-      res.redirect(response.status, redirectUrl);
+//       res.redirect(response.status, redirectUrl);
 
-      return;
-    }
+//       return;
+//     }
 
-    res.setHeader(header, resHeaders[header]);
-  }
+//     res.setHeader(header, resHeaders[header]);
+//   }
 
-  res.status(response.status);
+//   res.status(response.status);
 
-  response.data.pipe(res);
-});
+//   response.data.pipe(res);
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
